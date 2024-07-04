a6728f8faae1b3f397f1fe5a94003861a3f55e05
Jessica Kerr <jessitron@gmail.com>
2024-07-03 21:01:27 -0500

    Use the dependency

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!(
@@ -15,5 +16,9 @@ fn main() {
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
     println!("You guessed: {guess}");
-    
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is {secret_number}");
+
 }
