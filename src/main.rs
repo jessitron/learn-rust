e635b9c70f1830ee3c2ea6528e4b625bd578abab
Jessica Kerr <jessitron@gmail.com>
2024-07-03 21:10:58 -0500

    Doing things wrong: use string comparison

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!(
@@ -20,5 +21,16 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("The secret number is {secret_number}");
+
+    let as_str = ToString::to_string("{secret_number}");
+
+    let result = match guess.cmp(&as_str) {
+        Ordering::Less => "Too small",
+        Ordering::Equal => "CORRECT",
+        Ordering::Greater => "Too big",
+    };
+
+    println!("{result}");
+
 
 }
